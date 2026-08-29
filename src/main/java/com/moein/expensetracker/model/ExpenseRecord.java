47e4b955e0e7b716ff7a1e013dea05f8783b0603
Moein Moradi <moradimoein76@gmail.com>
2026-08-29 21:18:07 +0200

    Implement expense amount validation

---
+++ src/main/java/com/moein/expensetracker/model/ExpenseRecord.java
@@ -0,0 +1,11 @@
+package com.moein.expensetracker.model;
+
+import java.time.LocalDate;
+
+public class ExpenseRecord {
+
+	public ExpenseRecord(String description, double amount, String category, LocalDate date) {
+
+		throw new IllegalArgumentException();
+	}
+}
