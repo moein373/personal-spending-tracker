9d59a6165d3189a9a4d401821c4c5f59522f9415
Moein Moradi <moradimoein76@gmail.com>
2026-08-29 21:30:49 +0200

    Add failing test for blank expense description

--- src/test/java/com/moein/expensetracker/model/ExpenseRecordTest.java
+++ src/test/java/com/moein/expensetracker/model/ExpenseRecordTest.java
@@ -33,4 +33,10 @@ public class ExpenseRecordTest {
 		assertThrows(IllegalArgumentException.class,
 				() -> new ExpenseRecord("", 10.0, "Food", LocalDate.of(2026, 8, 29)));
 	}
+
+	@Test
+	public void constructorWithWhitespaceDescriptionShouldThrow() {
+		assertThrows(IllegalArgumentException.class,
+				() -> new ExpenseRecord("   ", 10.0, "Food", LocalDate.of(2026, 8, 29)));
+	}
 }
