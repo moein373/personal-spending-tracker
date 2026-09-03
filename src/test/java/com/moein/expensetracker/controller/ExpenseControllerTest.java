27f707910ffc12f959207b19f806d3d93c3acc3a
Moein Moradi <moradimoein76@gmail.com>
2026-09-03 17:00:42 +0200

    Add failing test for loading all expenses

--- src/test/java/com/moein/expensetracker/controller/ExpenseControllerTest.java
+++ src/test/java/com/moein/expensetracker/controller/ExpenseControllerTest.java
@@ -13,6 +13,9 @@ import org.mockito.junit.MockitoJUnitRunner;
 import com.moein.expensetracker.model.ExpenseRecord;
 import com.moein.expensetracker.repository.ExpenseRepository;
 import com.moein.expensetracker.ui.ExpenseView;
+import static org.mockito.Mockito.when;
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ExpenseControllerTest {
@@ -38,5 +41,20 @@ public class ExpenseControllerTest {
 
 		verify(expenseRepository).save(expense);
 		verify(expenseView).expenseAdded(expense);
+	}
+
+	@Test
+	public void shouldLoadAllExpenses() {
+		ExpenseRecord expense1 = new ExpenseRecord("1", "Coffee", 3.50, "Food", LocalDate.of(2026, 9, 3));
+
+		ExpenseRecord expense2 = new ExpenseRecord("2", "Bus", 1.50, "Transport", LocalDate.of(2026, 9, 3));
+
+		List<ExpenseRecord> expenses = Arrays.asList(expense1, expense2);
+
+		when(expenseRepository.findAll()).thenReturn(expenses);
+
+		expenseController.loadAllExpenses();
+
+		verify(expenseView).showAllExpenses(expenses);
 	}
 }
