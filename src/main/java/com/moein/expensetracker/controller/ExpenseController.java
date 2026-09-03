6363b50569767e6776e83b7fc3f7e238d2b55e15
Moein Moradi <moradimoein76@gmail.com>
2026-09-03 15:53:48 +0200

    Implement expense controller add behavior

---
+++ src/main/java/com/moein/expensetracker/controller/ExpenseController.java
@@ -0,0 +1,21 @@
+package com.moein.expensetracker.controller;
+
+import com.moein.expensetracker.model.ExpenseRecord;
+import com.moein.expensetracker.repository.ExpenseRepository;
+import com.moein.expensetracker.ui.ExpenseView;
+
+public class ExpenseController {
+
+	private ExpenseView expenseView;
+	private ExpenseRepository expenseRepository;
+
+	public ExpenseController(ExpenseView expenseView, ExpenseRepository expenseRepository) {
+		this.expenseView = expenseView;
+		this.expenseRepository = expenseRepository;
+	}
+
+	public void addExpense(ExpenseRecord expense) {
+		expenseRepository.save(expense);
+		expenseView.expenseAdded(expense);
+	}
+}
