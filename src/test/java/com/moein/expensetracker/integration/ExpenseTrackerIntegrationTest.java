f1b8713d24ecb18a595c3f9bfe0fa7b404283b9a
Moein Moradi <moradimoein76@gmail.com>
2026-09-04 09:25:07 +0200

    Add UI integration test skeleton

---
+++ src/test/java/com/moein/expensetracker/integration/ExpenseTrackerIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.moein.expensetracker.integration;
+
+import org.junit.Test;
+
+public class ExpenseTrackerIntegrationTest {
+
+	@Test
+	public void shouldStartIntegrationTest() {
+	}
+}
